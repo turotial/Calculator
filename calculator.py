9868fa4c1083a62bf0ed383dfd9f7ea9c446bbeb
Nagaraju <122417099+turotial@users.noreply.github.com>
2025-07-28 15:29:43 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,7 @@
 from tkinter import *
 from tkinter.messagebox import showerror
-
 def add_text(text):
     entry_var.set(entry_var.get() + text)
-
 def calculate():
     try:
         expr = entry_var.get().replace('xx', '**') 
@@ -11,22 +9,16 @@ def calculate():
         entry_var.set(str(result))
     except:
         showerror("Error", "Invalid Expression!")
-
 def clear():
     entry_var.set("")
-
 def backspace():
     entry_var.set(entry_var.get()[:-1])
-
 root = Tk()
 root.title("Simple Calculator")
 root.geometry("250x350")
 root.resizable(False, False)
-
 entry_var = StringVar()
-
 Entry(root, textvariable=entry_var, font=("Arial", 18), justify="right").pack(fill="both", padx=10, pady=10)
-
 buttons = [
     ['7', '8', '9', '/'],
     ['4', '5', '6', '*'],
@@ -34,7 +26,6 @@ buttons = [
     ['0', '.', 'xx', '+'],
     ['AC', 'C', '=', '()']
 ]
-
 for row in buttons:
     frame = Frame(root)
     frame.pack(expand=True, fill="both")
@@ -48,5 +39,4 @@ for row in buttons:
             add_text(b)
         )
         Button(frame, text=btn, font=("Arial", 14), command=action).pack(side="left", expand=True, fill="both")
-
 root.mainloop()
